6fdde95d4f9c90ce8f57e66b821a31afba97e0f3
PatBelton <patrick.belton@protonmail.com>
2021-09-05 22:59:10 -0400

    added "do_action_mask_K4": this will mask out the "keep 4 singletons" action that don't help for getting a straight.  Now the non-masked actions are those for which you have four singletons and just missing one dice for a straight

--- test.py
+++ test.py
@@ -10,3 +10,19 @@ myScore = Score()
 myDice.roll_Heavy()
 print(myDice)
 print(myDice.sum())
+
+myDice.set([1, 1, 4, 5, 6])
+
+if len(collections.Counter(myDice.dice())) == 4:
+    singletons = list(myDice.as_set())
+    mask = '0'
+    for i in range(len(singletons)-1):
+        print(singletons[i])
+        if singletons[i+1] - singletons[i] > 2:
+            mask = '1'
+    print("mask = ", mask)
+
+
+action_table = action_q_table()
+# keep actions plus masks:
+action_to_dice_to_keep, keeping_actions_masks = action_table.print_all_action_q_table()
