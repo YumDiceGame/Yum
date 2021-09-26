ed1064b2fec7ac7fbaa3c852818549191543a19d
PatBelton <patrick.belton@protonmail.com>
2021-09-26 15:43:37 -0400

    Back to across the line with a good score: 140.275 LEARNING_RATE = 0.2 DISCOUNT = 0.725 above the line NUM_EPISODES = 6_000_000

--- constants.py
+++ constants.py
@@ -10,11 +10,11 @@ NUM_SCORE_CAT_ABOVE_LINE = 6
 ABOVE_THE_LINE_CATEGORIES = ['Ones', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes']
 # Column numbers of categories in score table
 YUM_COL = 6 # Cat7 - 1 for pos
-LEARNING_RATE = 0.1  # was 0.1 till 9/21/21
-DISCOUNT = 0.75  # was 0.95 till 9/21/21
+LEARNING_RATE = 0.2  # was 0.1 till 9/25/21  the 0.2 is really new
+DISCOUNT = 0.725  # was 0.95 till 9/21/21.  0.75 gave great results for above the line
 NUM_EPISODES = 6_000_000  # 50_000_000 for keeping / 6_000_000 for scoring (no eps) then 12M with eps on top
 EVAL_Q_TABLE = 5_000  # time to evaluate q table for convergence
-NUM_GAMES = 1000
+NUM_GAMES = 100
 NUM_GAMES_SOLO = 1
 NUM_KEEPING_ACTIONS = 61
 Q_TABLE_SAVE_INTERVAL = 10_000_000
